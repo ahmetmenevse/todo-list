2983d5282bca89db7fece023d2bce0aaa346e3fd
Ahmet MENEVSE <ahmetmenevse@Ahmets-MBP.gateway>
2023-10-30 12:44:52 +0800

    designed index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
-
-  def show  
-  end
-
 
   def new
     @task = Task.new
