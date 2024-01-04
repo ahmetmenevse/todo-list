bb46761eb1b85062ae017e0832eaed14f9de3e01
ahmet menevse <ahmetmenevse26@gmail.com>
2024-01-04 14:39:50 +0800

    model callbacks created for capitalizing task name and validates for name, description, date, status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
   enum status: { to_do: 'ToDo', in_progress: 'In Progress', done: 'Done' }
+  validates :name, presence: true, uniqueness: { case_sensitive: false, message: 'is already taken, please change the name' }
+  validates :description, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :date, presence: true
+  validates :status, presence: true
+
+  before_create :capitalize_name
+
+  private
+
+  def capitalize_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
 end
