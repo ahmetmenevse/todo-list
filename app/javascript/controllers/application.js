b5954d2353f10bd36ee45ea96dd0c186dad27b19
ahmet menevse <ahmetmenevse26@gmail.com>
2024-01-09 13:30:14 +0800

    fixed stimulus controllers  path

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,8 @@
 import { Application } from "@hotwired/stimulus"
-import  tooltip  from "./tooltip_controller"
-
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
-Stimulus.register("tooltip", tooltip)
 export { application }
